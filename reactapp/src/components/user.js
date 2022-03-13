a06ab020dbfd61dbc9d7c4259bb5ed73a60c99c5
yavuzyarkinokular <yarkinokular@gmail.com>
2022-03-13 22:48:54 +0300

    button ve form eklendi

--- reactapp/src/components/user.js
+++ reactapp/src/components/user.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 class User extends Component {
-    render() {
-        return <div >
-            <
-            form type => < /form></div > ;
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text"></input>
+          <button> Gönder</button>
+        </form>
+      </div>
+    );
+  }
 }
 export default User;
