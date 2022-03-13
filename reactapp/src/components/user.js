6daca5310a57654606b9bb2675aa6b8677813a55
yavuzyarkinokular <yarkinokular@gmail.com>
2022-03-13 13:13:32 +0300

    first component

---
+++ reactapp/src/components/user.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class User extends Component {
+    render() {
+        return <div >
+            <
+            form type => < /form></div > ;
+    }
+}
+export default User;
