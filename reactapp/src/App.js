e8af490075ae192ca99666cddadcea32cf0e6658
yavuzyarkinokular <yarkinokular@gmail.com>
2022-03-13 23:35:12 +0300

    header class ile dahil edildi

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,11 +5,7 @@ import User from "./components/user";
 function App() {
   return (
     <div className="App">
-      <h4>App Component</h4>
-      <User></User>
-      <User></User>
-      <User></User>
-      <User></User>
+      <h4 className="header"> App components</h4>
     </div>
   );
 }
