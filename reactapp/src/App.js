dd8470dd8cdd1cb7440af8e261a6ddd40b079832
yavuzyarkinokular <yarkinokular@gmail.com>
2022-03-14 00:06:43 +0300

    bootstrap test

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,7 +4,7 @@ import User from "./components/user";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h4 className="header"> App components</h4>
     </div>
   );
