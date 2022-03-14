051356ece1115aed2f0916a0527640cbc9dd175f
yavuzyarkinokular <yarkinokular@gmail.com>
2022-03-14 22:02:03 +0300

    static props

--- reactapp/src/components/user.js
+++ reactapp/src/components/user.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class User extends Component {
+  static defaultProps = {
+    name: "Your Name",
+    mail: "Your Mail",
+    location: "Your Location",
+  };
   render() {
     const { name, mail, location } = this.props;
 
     return (
       <div>
         <ul>
-          <li>{name}</li>
-          <li>{mail} </li>
-          <li>{location} </li>
+          <li>İsminiz: {name}</li>
+          <li>Mail Adresiniz: {mail} </li>
+          <li>Yaşadığınız şehir: {location} </li>
         </ul>
       </div>
     );
@@ -21,9 +26,5 @@ User.propTypes = {
   mail: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
 };
-User.defaultProps = {
-  name: "Your Name",
-  mail: "Your Mail",
-  location: "Your Location",
-};
+User.defaultProps = {};
 export default User;
