88d9ff4bc8279be31e19451a717025cf0055e996
yavuzyarkinokular <yarkinokular@gmail.com>
2022-03-17 14:13:55 +0300

    state method

--- reactapp/src/components/user.js
+++ reactapp/src/components/user.js
@@ -2,25 +2,38 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class User extends Component {
+  state = {
+    isVisible: true,
+  };
   static defaultProps = {
     name: "Your Name",
     mail: "Your Mail",
     location: "Your Location",
   };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isVisible: false,
+  //   };
+  // }
   render() {
+    //destructing
     const { name, mail, location } = this.props;
+    const { isVisible } = this.state;
 
     return (
-      <div>
+      <div className="col-md-8 mb-4">
         <div className="card">
           <div className="card-header d-flex justify-content-between">
             <h4 className="d-inline"> {name}</h4>
           </div>
         </div>
-        <div className="card-body">
-          <p className="card-text">{mail} </p>
-          <p className="card-text">{location} </p>
-        </div>
+        {isVisible ? (
+          <div className="card-body">
+            <p className="card-text">{mail} </p>
+            <p className="card-text">{location} </p>
+          </div>
+        ) : null}
       </div>
     );
   }
